64f590e631226822be2f13ec7723e4fc249ad90d
Leonardo Cristella <leonardo.cristella@cern.ch>
2020-07-10 17:19:31 +0200

    Weighted average between HGCAL and MTD timings

--- RecoHGCal/TICL/plugins/PFTICLProducer.cc
+++ RecoHGCal/TICL/plugins/PFTICLProducer.cc
@@ -23,20 +23,37 @@ public:
   void produce(edm::StreamID, edm::Event&, const edm::EventSetup&) const override;
 
 private:
+  // parameters
+  const bool useTimingQuality_;
+  const float timingQualityThreshold_;
+
   // inputs
   const edm::EDGetTokenT<edm::View<TICLCandidate>> ticl_candidates_;
+  const edm::EDGetTokenT<edm::ValueMap<float>> srcTrackTime_, srcTrackTimeError_, srcTrackTimeQuality_;
 };
 
 DEFINE_FWK_MODULE(PFTICLProducer);
 
 PFTICLProducer::PFTICLProducer(const edm::ParameterSet& conf)
-    : ticl_candidates_(consumes<edm::View<TICLCandidate>>(conf.getParameter<edm::InputTag>("ticlCandidateSrc"))) {
+    : useTimingQuality_(conf.existsAs<edm::InputTag>("trackTimeQualityMap")),
+      timingQualityThreshold_(useTimingQuality_ ? conf.getParameter<double>("timingQualityThreshold") : -99.),
+      ticl_candidates_(consumes<edm::View<TICLCandidate>>(conf.getParameter<edm::InputTag>("ticlCandidateSrc"))),
+      srcTrackTime_(consumes<edm::ValueMap<float>>(conf.getParameter<edm::InputTag>("trackTimeValueMap"))),
+      srcTrackTimeError_(consumes<edm::ValueMap<float>>(conf.getParameter<edm::InputTag>("trackTimeErrorMap"))),
+      srcTrackTimeQuality_(useTimingQuality_
+                           ? consumes<edm::ValueMap<float>>(conf.getParameter<edm::InputTag>("trackTimeQualityMap"))
+                           : edm::EDGetTokenT<edm::ValueMap<float>>())
+ {
   produces<reco::PFCandidateCollection>();
 }
 
 void PFTICLProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
   desc.add<edm::InputTag>("ticlCandidateSrc", edm::InputTag("ticlCandidateFromTracksters"));
+  desc.add<edm::InputTag>("trackTimeValueMap", edm::InputTag("tofPID:t0"));
+  desc.add<edm::InputTag>("trackTimeErrorMap", edm::InputTag("tofPID:sigmat0"));
+  desc.add<edm::InputTag>("trackTimeQualityMap", edm::InputTag("mtdTrackQualityMVA:mtdQualMVA"));
+  desc.add<double>("timingQualityThreshold", 0.5);
   descriptions.add("pfTICLProducer", desc);
 }
 
@@ -45,6 +62,13 @@ void PFTICLProducer::produce(edm::StreamID, edm::Event& evt, const edm::EventSet
   edm::Handle<edm::View<TICLCandidate>> ticl_cand_h;
   evt.getByToken(ticl_candidates_, ticl_cand_h);
   const auto ticl_candidates = *ticl_cand_h;
+
+  edm::Handle<edm::ValueMap<float>> trackTimeH, trackTimeErrH, trackTimeQualH;
+  evt.getByToken(srcTrackTime_, trackTimeH);
+  evt.getByToken(srcTrackTimeError_, trackTimeErrH);
+  if (useTimingQuality_) {
+    evt.getByToken(srcTrackTimeQuality_, trackTimeQualH);
+  }
 
   auto candidates = std::make_unique<reco::PFCandidateCollection>();
 
@@ -84,7 +108,21 @@ void PFTICLProducer::produce(edm::StreamID, edm::Event& evt, const edm::EventSet
       reco::TrackRef ref(ticl_cand.trackPtr().id(), int(ticl_cand.trackPtr().key()), &evt.productGetter());
       candidate.setTrackRef(ref);
     }
-    candidate.setTime(ticl_cand.time(), ticl_cand.timeError());
+
+    auto time = ticl_cand.time();
+    auto timeE = ticl_cand.timeError();
+    // Compute weighted average between HGCAL and MTD timing if available
+    if (candidate.charge()) {
+      const bool assocQuality = (*trackTimeQualH)[candidate.trackRef()] > timingQualityThreshold_;
+      if (assocQuality) {
+        const auto timeMTD = (*trackTimeH)[candidate.trackRef()];
+        const auto timeEMTD = (*trackTimeErrH)[candidate.trackRef()];
+
+        timeE = 1 / (pow(timeE,-2) + pow(timeEMTD,-2));
+        time = (time/pow(timeE,2) + timeMTD/pow(timeEMTD,2)) * timeE;
+      }
+    }
+    candidate.setTime(time, timeE);
   }
 
   evt.put(std::move(candidates));
